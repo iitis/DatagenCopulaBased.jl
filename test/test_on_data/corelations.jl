5e258598510463065ceab48901a94cabded1f3d1
kdomino <kdomino@iitis.pl>
2020-02-20 12:52:30 +0100

    move tests on data

--- test/test_on_data/corelations.jl
+++ test/test_on_data/corelations.jl
@@ -1,5 +1,5 @@
-using QuadGK
-using Cubature
+#!/usr/bin/env julia
+
 using DatagenCopulaBased
 import DatagenCopulaBased: ρ2θ
 using Distributions
@@ -7,20 +7,24 @@ using Cumulants
 using PyPlot
 using SymmetricTensors
 using StatsBase
-using NLsolve
-using LsqFit
 using PyCall
 using JLD2
 using FileIO
+using Distributed
 
-@pyimport matplotlib as mpl
+#using QuadGK
+#using Cubature
+#using NLsolve
+#using LsqFit
+
+mpl = pyimport("matplotlib")
 mpl.rc("text", usetex=true)
 
-addprocs(9)
+addprocs(3)
 @everywhere using DatagenCopulaBased
 @everywhere using Cumulants
 nprocs()
-# Cumulants
+
 
 function ccl(u::Vector, θ::Union{Int, Float64})
   q = length(u)
@@ -89,24 +93,24 @@ end
 function statstheoreticalclayton(m::Int = 3, d = Normal(0,1))
   θ = map(i -> ρ2θ(i, "clayton"), 0.05:0.05:.95)
   n = length(θ)
-  t = (m == 3)? zeros(n, 3): zeros(n, 5)
+  t = (m == 3) ? zeros(n, 3) : zeros(n, 5)
   for i in 1:n
-    t[i,:] = (m == 3)? c3symmarg(θ[i], ccl, d): c4symmarg(θ[i], ccl, d)
+    t[i,:] = (m == 3) ? c3symmarg(θ[i], ccl, d) : c4symmarg(θ[i], ccl, d)
     println(i)
   end
   t
 end
 
-save("c3clgmarg.jld2", "emp", e, "theoret", t, "rho", ρ)
+#save("c3clgmarg.jld2", "emp", e, "theoret", t, "rho", ρ)
 
 function empiricalcums(copula::String, m::Int, ρ::Vector{Float64}, rev = false)
   θ = map(i -> ρ2θ(i, copula), ρ)
   println(θ)
   n = length(θ)
-  e = (m == 3)? zeros(n, 3): zeros(n, 5)
+  e = (m == 3) ? zeros(n, 3) : zeros(n, 5)
   for i in 1:n
     println(i)
-    e[i,:] = (m == 3)? c3empirical(θ[i], copula, Normal(0,1), rev): c4empirical(θ[i], copula, Normal(0,1), rev)
+    e[i,:] = (m == 3) ? c3empirical(θ[i], copula, Normal(0,1), rev) : c4empirical(θ[i], copula, Normal(0,1), rev)
   end
   e
 end
@@ -115,9 +119,9 @@ end
 function plotc3empth(e,t, ρ)
   mpl.rc("font", family="serif", size = 7)
   fig, ax = subplots(figsize = (2.5, 2.))
-  fx = matplotlib[:ticker][:ScalarFormatter]()
-  fx[:set_powerlimits]((-1, 4))
-  ax[:yaxis][:set_major_formatter](fx)
+  fx = matplotlib.ticker.ScalarFormatter()
+  fx.set_powerlimits((-1, 4))
+  ax.yaxis.set_major_formatter(fx)
   plot(ρ, e[:,3], "o", color = "black", label = "\$ \\mathbf{i} = (1,2,3) \$", markersize = 1.)
   plot(ρ, t[:,3], color = "black", linewidth = 0.5)
   plot(ρ, e[:,2], "o", color = "blue", label = "\$ \\mathbf{i} = (1,1,2) \$", markersize = 1.)
@@ -126,60 +130,35 @@ function plotc3empth(e,t, ρ)
   plot(ρ, t[:,1], color = "red", linewidth = 0.5)
   PyPlot.ylabel("cumulant element", labelpad = -1.0)
   PyPlot.xlabel("Spearman \$ \\rho \$ between marginals", labelpad = -1.0)
-  ax[:legend](fontsize = 6, loc = 3, ncol = 1)
+  ax.legend(fontsize = 6, loc = 3, ncol = 1)
   subplots_adjust(left = 0.12, bottom = 0.12)
-  fig[:savefig]("pics/c3clgmarg.pdf")
+  fig.savefig("pics/c3clgmarg.pdf")
 end
 
 
 function plotc3emp(e, ρ, cop::String)
   mpl.rc("font", family="serif", size = 7)
   fig, ax = subplots(figsize = (2.5, 2.))
-  fx = matplotlib[:ticker][:ScalarFormatter]()
-  fx[:set_powerlimits]((-1, 4))
-  ax[:yaxis][:set_major_formatter](fx)
+  fx = matplotlib.ticker.ScalarFormatter()
+  fx.set_powerlimits((-1, 4))
+  ax.yaxis.set_major_formatter(fx)
   plot(ρ, e[:,3], "o", color = "black", label = "\$ \\mathbf{i} = (1, 2, 3) \$", markersize = 1.)
   plot(ρ, e[:,2], "o", color = "blue", label = "\$ \\mathbf{i} = (1, 1, 2) \$", markersize = 1.)
   plot(ρ, e[:,1], "o", color = "red", label = "\$ \\mathbf{i} = (1, 1, 1) \$", markersize = 1.)
   PyPlot.ylabel("cumulant element", labelpad = -1.0)
   PyPlot.xlabel("Spearman \$ \\rho \$ between marginals", labelpad = -1.0)
-  #ax[:legend](fontsize = 7, loc = 3, ncol = 1)
+  #ax.legend(fontsize = 7, loc = 3, ncol = 1)
   subplots_adjust(left = 0.16, bottom = 0.12)
-  fig[:savefig]("pics/c3"*cop*"gmarg.pdf")
+  fig.savefig("pics/c3"*cop*"gmarg.pdf")
 end
-
-
-c3els = load("pics/c3clgmarg.jld2")
-plotc3empth(c3els["emp"],c3els["theoret"], c3els["rho"])
-
-ρ = [i for i in 0.02:0.02:.85]
-e = empiricalcums("frank", 3, ρ)
-save("pics/c3frgmarg.jld2", "emp", e,"rho", ρ)
-
-
-
-ρ = [i for i in 0.02:0.02:.98]
-e = empiricalcums("gumbel", 3, ρ)
-save("pics/c3gugmarg.jld2", "emp", e,"rho", ρ)
-c3els = load("pics/c3gugmarg.jld2")
-plotc3emp(c3els["emp"], c3els["rho"], "gu")
-
-
-ρ = [i for i in 0.02:0.02:.5]
-e = empiricalcums("amh", 3, ρ)
-save("pics/c3amhgmarg.jld2", "emp", e,"rho", ρ)
-c3els = load("pics/c3amhgmarg.jld2")
-plotc3emp(c3els["emp"], c3els["rho"], "amh")
-
-
 
 
 function plotc4emp(e, ρ, cop::String)
   mpl.rc("font", family="serif", size = 7)
   fig, ax = subplots(figsize = (2.5, 2.))
-  fx = matplotlib[:ticker][:ScalarFormatter]()
-  fx[:set_powerlimits]((-1, 4))
-  ax[:yaxis][:set_major_formatter](fx)
+  fx = matplotlib.ticker.ScalarFormatter()
+  fx.set_powerlimits((-1, 4))
+  ax.yaxis.set_major_formatter(fx)
   plot(ρ, e[:,5], "o", color = "black", label = "\$ i_1, i_2, i_3, i_4 \$", markersize = 1.)
   plot(ρ, e[:,4], "o", color = "brown", label = "\$ i_1, i_1, i_2, i_3 \$", markersize = 1.)
   plot(ρ, e[:,3], "o", color = "gray", label = "\$ i_1, i_1, i_2, i_2 \$", markersize = 1.)
@@ -187,17 +166,17 @@ function plotc4emp(e, ρ, cop::String)
   plot(ρ, e[:,1], "o", color = "red", label = "\$ i_1, i_1, i_1, i_1 \$", markersize = 1.)
   PyPlot.ylabel("cumulant element", labelpad = -1.0)
   PyPlot.xlabel("Spearman \$ \\rho \$", labelpad = -1.0)
-  #ax[:legend](fontsize = 4.5, loc = 3, ncol = 1)
+  #ax.legend(fontsize = 4.5, loc = 3, ncol = 1)
   subplots_adjust(left = 0.16, bottom = 0.12)
-  fig[:savefig]("pics/c4"*cop*"gmarg.pdf")
+  fig.savefig("pics/c4"*cop*"gmarg.pdf")
 end
 
 function plotc4empth(e,t, ρ, ρ1)
   mpl.rc("font", family="serif", size = 7)
   fig, ax = subplots(figsize = (2.5, 2.))
-  fx = matplotlib[:ticker][:ScalarFormatter]()
-  fx[:set_powerlimits]((-1, 4))
-  ax[:yaxis][:set_major_formatter](fx)
+  fx = matplotlib.ticker.ScalarFormatter()
+  fx.set_powerlimits((-1, 4))
+  ax.yaxis.set_major_formatter(fx)
   plot(ρ1, t[:,5], color = "black", linewidth = 0.5)
   plot(ρ, e[:,5], "o", color = "black", label = "\$ i_1, i_2, i_3, i_4 \$", markersize = 1.)
   plot(ρ1, t[:,4], color = "brown", linewidth = 0.5)
@@ -210,28 +189,44 @@ function plotc4empth(e,t, ρ, ρ1)
   plot(ρ1, t[:,1], color = "red", linewidth = 0.5)
   PyPlot.ylabel("cumulant element", labelpad = -1.0)
   PyPlot.xlabel("Spearman \$ \\rho \$", labelpad = -1.0)
-  ax[:legend](fontsize = 4.5, loc = 3, ncol = 1)
+  ax.legend(fontsize = 4.5, loc = 3, ncol = 1)
   subplots_adjust(left = 0.12, bottom = 0.12)
-  fig[:savefig]("pics/c4clgmarg.pdf")
+  fig.savefig("pics/c4clgmarg.pdf")
 end
+
+
+if false
+  ρ = collect(0.05:0.05:.95)
+  e4 = empiricalcums("gumbel", 4, ρ, false)
+  save("pics/c4gugmarg.jld2", "emp", e4,"rho", ρ)
+  t = statstheoreticalclayton(4, Normal(0,1));
+  save("pics/teorc4clgmarg.jld2", "theor", t,"rho", ρ)
+
+  ρ = [i for i in 0.02:0.02:.85]
+  e = empiricalcums("frank", 3, ρ)
+  save("pics/c3frgmarg.jld2", "emp", e,"rho", ρ)
+
+  ρ = [i for i in 0.02:0.02:.98]
+  e = empiricalcums("gumbel", 3, ρ)
+  save("pics/c3gugmarg.jld2", "emp", e,"rho", ρ)
+
+  ρ = [i for i in 0.02:0.02:.5]
+  e = empiricalcums("amh", 3, ρ)
+  save("pics/c3amhgmarg.jld2", "emp", e,"rho", ρ)
+end
+
+
+c3els = load("pics/c3clgmarg.jld2")
+plotc3empth(c3els["emp"],c3els["theoret"], c3els["rho"])
+
+c3els = load("pics/c3gugmarg.jld2")
+plotc3emp(c3els["emp"], c3els["rho"], "gu")
+
+c3els = load("pics/c3amhgmarg.jld2")
+plotc3emp(c3els["emp"], c3els["rho"], "amh")
 
 c4els = load("pics/c4clgmarg.jld2")
 plotc4emp(c4els["emp"], c4els["rho"], "cl")
-
-
-a = c4symmarg(4., ccl, Normal(0,1))
-
-b = c4empirical(4., "clayton", Normal(0,1));
-
-
-ρ = collect(0.05:0.05:.95)
-
-e4 = empiricalcums("gumbel", 4, ρ, false)
-save("pics/c4gugmarg.jld2", "emp", e4,"rho", ρ)
-
-t = statstheoreticalclayton(4, Normal(0,1));
-
-save("pics/teorc4clgmarg.jld2", "theor", t,"rho", ρ)
 
 emp = load("pics/c4clgmarg.jld2")
 t = load("pics/teorc4clgmarg.jld2")
--- test/test_on_data/plotcors.jl
+++ test/test_on_data/plotcors.jl
@@ -1,15 +1,17 @@
-using Cubature
+#!/usr/bin/env julia
+
+using HCubature
 using Distributions
 using PyCall
-@pyimport matplotlib as mpl
+mpl = pyimport("matplotlib")
 mpl.rc("text", usetex=true)
 mpl.use("Agg")
-@pyimport matplotlib.ticker as mti
+mti = pyimport("matplotlib.ticker")
 using PyPlot
 
-Cg(x::Vector{Float64}, θ::Union{Int, Float64}) = exp(-((-log(x[1]))^θ+(-log(x[2]))^θ)^(1/θ))
+Cg(x, θ::Union{Int, Float64}) = exp(-((-log(x[1]))^θ+(-log(x[2]))^θ)^(1/θ))
 
-function Ccl(x::Vector{Float64}, θ::Union{Int, Float64})
+function Ccl(x, θ::Union{Int, Float64})
   if θ > 0
     return (x[1]^(-θ)+x[2]^(-θ)-1)^(-1/θ)
   else
@@ -17,11 +19,11 @@ function Ccl(x::Vector{Float64}, θ::Union{Int, Float64})
   end
 end
 
-Cf(x::Vector{Float64}, θ::Union{Int, Float64}) =
+Cf(x, θ::Union{Int, Float64}) =
   -log(1+((exp(-θ*x[1])-1)*(exp(-θ*x[2])-1))/(exp(-θ)-1))/θ
 
 
-Camh(x::Vector{Float64}, θ::Union{Int, Float64}) = x[1]*x[2]/(1-θ*(1-x[1])*(1-x[2]))
+Camh(x, θ::Union{Int, Float64}) = x[1]*x[2]/(1-θ*(1-x[1])*(1-x[2]))
 
 function getcov(copula::Function, d, θa::Vector{Float64})
   covs = Float64[]
@@ -47,17 +49,17 @@ end
 function plotcor(copula::Function, θ::Vector{Float64})
   mpl.rc("font", family="serif", size = 7)
   fig, ax = subplots(figsize = (2.5, 2.))
-  ax[:plot](θ, getcov(copula, Normal(0,1), θ), label= "normal dist", color = "r", linewidth = 0.5)
-  ax[:plot](θ, getcov(copula, LogNormal(1,1), θ), label= "lognormal(1,1)", color = "blue", linewidth = 0.5)
-  ax[:plot](θ, getcov(copula, Arcsine(0,1), θ), label= "arcsine(0,1)", color = "green", linewidth = 0.5)
-  ax[:plot](θ, getcov(copula, TDist(5), θ), label= "tdist(5)", color = "grey", linewidth = 0.5)
-  ax[:plot](θ, getcov(copula, Weibull(1,1), θ), label= "weibull(1,1)", color = "yellow", linewidth = 0.5)
-  ax[:plot](θ, getρ(copula, θ), label = "uniform dist", color = "black", linewidth = 0.5)
-  ax[:legend](fontsize = 4.5, loc = 4, ncol = 1)
+  ax.plot(θ, getcov(copula, Normal(0,1), θ), label= "normal dist", color = "r", linewidth = 0.5)
+  ax.plot(θ, getcov(copula, LogNormal(1,1), θ), label= "lognormal(1,1)", color = "blue", linewidth = 0.5)
+  ax.plot(θ, getcov(copula, Arcsine(0,1), θ), label= "arcsine(0,1)", color = "green", linewidth = 0.5)
+  ax.plot(θ, getcov(copula, TDist(5), θ), label= "tdist(5)", color = "grey", linewidth = 0.5)
+  ax.plot(θ, getcov(copula, Weibull(1,1), θ), label= "weibull(1,1)", color = "yellow", linewidth = 0.5)
+  ax.plot(θ, getρ(copula, θ), label = "uniform dist", color = "black", linewidth = 0.5)
+  ax.legend(fontsize = 4.5, loc = 4, ncol = 1)
   subplots_adjust(bottom = 0.16, top=0.92, left = 0.18, right = 0.92)
   PyPlot.ylabel("Pearson correlation", labelpad = -1.5)
   PyPlot.xlabel("\$ \\theta \$", labelpad = 0.6)
-  fig[:savefig]("pics/cor"*string(copula)*".eps")
+  fig.savefig("pics/cor"*string(copula)*".pdf")
 end
 
 plotcor(Cg, vcat([1.01], [i for i in 1.2:0.2:10.]))
