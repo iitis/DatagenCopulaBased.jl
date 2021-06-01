f36246bd2ad9a3f72b1c8ee5c7eccf6ba647728d
Krzysztof Domino <kdomino@iitis.pl>
2021-06-01 12:26:49 +0200

    move plotters

--- benchmarks/scatterplots/datagen.jl
+++ benchmarks/scatterplots/datagen.jl
@@ -31,10 +31,10 @@ ct = Student_cop(Σ, 1)
 t = 100_000
 
 r = Xoroshiro128Plus()
-x = simulate_copula(t, cch; rng = r)
+x = simulate_copula(t, cc; rng = r)
 npzwrite("copula.npy", x)
 
 Σ = [1. 0.8 0.6 0.6; 0.8 1. 0.6 0.6; 0.6 0.6 1. 0.6; 0.6 0.6 0.6 1.]
 x = Array(transpose(rand(MvNormal(Σ),t)))
-y = gcop2arch(x, ["gumbel"=>[1,2,3]])
+y = gcop2arch(x, ["clayton"=>[1,2,3]])
 npzwrite("transform.npy", y)
